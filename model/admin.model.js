7b253f3a6857f89417feeeb0183af9538455d7fa
sharmacs8853 <sharmacs8853@gmail.com>
2023-02-16 14:18:53 +0530

    admin schema changes

--- model/admin.model.js
+++ model/admin.model.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema({
-    name:{type: String, require: true},
-    email:{type: String, require: true},
-    password:{type: String, require: true, min:6},
+    name: { type: String, required: [true, "please enter your name"] },
+    email: { type: String, required: [true, "please Enter your email"] },
+    password: { type: String, required: [true, "password should be greater than 6 char"], min: 6 },
 })
 
 const adminModel = mongoose.model('admin', adminSchema);
 
-module.exports = {adminModel}
+module.exports = { adminModel }
